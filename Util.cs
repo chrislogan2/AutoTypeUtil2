15e0f2592233574580b33cc3eb94e42a9b0e3ff7
Chris Logan <logan.christopher@oscoit.com>
2025-08-12 16:25:49 -0300

    fix special char handling code in Util class.

--- Util.cs
+++ Util.cs
@@ -3,27 +3,21 @@ using System.Collections.Generic;
 using System.Diagnostics.Metrics;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace AutoTypeUtil2
 {
     internal class Util
     {
-        // SentText Hack: https://stackoverflow.com/questions/18291756/send-keys-special-characters-c-sharp
+        // SentText Hack:https://stackoverflow.com/questions/18299216/send-special-character-with-sendkeys
         // made marginally cleaner with .Contains
-        static char[] specialChars = { '{', '}', '(', ')', '+', '^' };
+        static String regexSendKeysPattern = "[+^%~()]";
+        static String regexSendKeysOutput = "{$0}";
         internal static void SendTextSpecialChars(string blockText)
         {
-            foreach (char letter in blockText)
-            {
-                bool _specialCharFound = false;
-                _specialCharFound=specialChars.Contains(letter) ? true : false;
-
-                if (_specialCharFound)
-                    SendKeys.Send("{" + letter.ToString() + "}");
-                else
-                    SendKeys.Send(letter.ToString());
-            }
+            string txt = Regex.Replace(blockText, regexSendKeysPattern, regexSendKeysOutput);
+            SendKeys.Send(txt);
         }
     }
 }
